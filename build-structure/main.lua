e5d61aede1fa370af5910ab1a9bc67cc57f15c04
Maxi <42847769+M4rethyu@users.noreply.github.com>
2023-08-17 18:24:27 +0200

    test file parsing

--- build-structure/main.lua
+++ build-structure/main.lua
@@ -1,4 +1,12 @@
+-- see if the file exists
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
 
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
 
 function lines_from(file)
   if not file_exists(file) then return {} end
