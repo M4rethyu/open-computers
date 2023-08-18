2216bc659be01ad1710b1e316a34dfa4c24ab7d6
Maxi <42847769+M4rethyu@users.noreply.github.com>
2023-08-18 20:28:48 +0200

    get initial movement pose from gps

--- build-structure/movement.lua
+++ build-structure/movement.lua
@@ -1,4 +1,6 @@
 local robot_api = require("robot")
+local gps
+pcall(function() gps = require("gps") end)
 
 
 local movement = {}
@@ -15,63 +17,67 @@ movement.DIRECTION = {
 
 
 -- track location and orientation
-movement.pose = {
-    x = 91,
-    y = 68,
-    z = 269,
-    dir = movement.DIRECTION.EAST
+local pose = {
+    x = 0,
+    y = 0,
+    z = 0,
+    dir = movement.DIRECTION.NORTH
 }
 
 -- getter and setter for pose
 function movement.getPose()
-    local pose = {
-        x = movement.pose.x,
-        y = movement.pose.y,
-        z = movement.pose.z,
-        dir = movement.pose.dir
+    local pose_copy = {
+        x = pose.x,
+        y = pose.y,
+        z = pose.z,
+        dir = pose.dir
     } -- make copy of pose so internal pose doesn't accidentally get changed
-    return pose
+    return pose_copy
 end
 
 function movement.setPose(x, y, z, dir) -- all parameters optional, use nil to omit left params
-    movement.pose.x = x or movement.pose.x
-    movement.pose.y = y or movement.pose.y
-    movement.pose.z = z or movement.pose.z
-    movement.pose.dir = dir or movement.pose.dir
+    pose.x = x or pose.x
+    pose.y = y or pose.y
+    pose.z = z or pose.z
+    pose.dir = dir or pose.dir
 end
 
+if gps then
+    local gps_pose = gps.position()
+    movement.setPose(gps_pose.x, gps_pose.y, gps_pose.z, gps_pose.dir)
+end
 
 -- wrappers for built in turning functions of robots to track orientation
 function movement.turnLeft()
     robot_api.turnLeft()
-    if movement.pose.dir == 0 then
-        movement.pose.dir = 3
-    else -- movement.pose.dir in {1, 2, 3}
-        movement.pose.dir = movement.pose.dir - 1
+    if pose.dir == 0 then
+        pose.dir = 3
+    else -- pose.dir in {1, 2, 3}
+        pose.dir = pose.dir - 1
     end
 end
 
 function movement.turnRight()
     robot_api.turnRight()
-    if movement.pose.dir == 3 then
-        movement.pose.dir = 0
-    else -- movement.pose.dir in {0, 1, 2}
-        movement.pose.dir = movement.pose.dir + 1
+    if pose.dir == 3 then
+        pose.dir = 0
+    else -- pose.dir in {0, 1, 2}
+        pose.dir = pose.dir + 1
     end
 end
 
 function movement.turnAround()
     robot_api.turnAround()
-    if movement.pose.dir == 0 or movement.pose.dir == 1 then
-        movement.pose.dir = movement.pose.dir + 2
-    else -- movement.pose.dir in {2, 3}
-        movement.pose.dir = movement.pose.dir - 2
+    if pose.dir == 0 or pose.dir == 1 then
+        pose.dir = pose.dir + 2
+    else -- pose.dir in {2, 3}
+        pose.dir = pose.dir - 2
     end
 end
 
 -- higher order turning function to go directly to target_facing
 function movement.turnTo(target_facing)
-  local diff_facing = (target_facing - movement.pose.dir)%4
+  local diff_facing = (target_facing - pose.dir)%4
   if diff_facing == 1 then
     movement.turnRight()
   elseif diff_facing == 2 then
@@ -86,14 +92,14 @@ end
 function movement.forward()
     local success, msg = robot_api.forward()
     if success then -- movement succeeded
-        if     movement.pose.dir == movement.DIRECTION.NORTH then
-            movement.pose.z = movement.pose.z - 1
-        elseif movement.pose.dir == movement.DIRECTION.EAST then
-            movement.pose.x = movement.pose.x + 1
-        elseif movement.pose.dir == movement.DIRECTION.SOUTH then
-            movement.pose.z = movement.pose.z + 1
-        elseif movement.pose.dir == movement.DIRECTION.WEST then
-            movement.pose.x = movement.pose.x - 1
+        if     pose.dir == movement.DIRECTION.NORTH then
+            pose.z = pose.z - 1
+        elseif pose.dir == movement.DIRECTION.EAST then
+            pose.x = pose.x + 1
+        elseif pose.dir == movement.DIRECTION.SOUTH then
+            pose.z = pose.z + 1
+        elseif pose.dir == movement.DIRECTION.WEST then
+            pose.x = pose.x - 1
         end
         return success
     else -- movement failed
@@ -104,14 +110,14 @@ end
 function movement.back()
     local success, msg = robot_api.back()
     if success then -- movement succeeded
-        if     movement.pose.dir == movement.DIRECTION.NORTH then
-            movement.pose.z = movement.pose.z + 1
-        elseif movement.pose.dir == movement.DIRECTION.EAST then
-            movement.pose.x = movement.pose.x - 1
-        elseif movement.pose.dir == movement.DIRECTION.SOUTH then
-            movement.pose.z = movement.pose.z - 1
-        elseif movement.pose.dir == movement.DIRECTION.WEST then
-            movement.pose.x = movement.pose.x + 1
+        if     pose.dir == movement.DIRECTION.NORTH then
+            pose.z = pose.z + 1
+        elseif pose.dir == movement.DIRECTION.EAST then
+            pose.x = pose.x - 1
+        elseif pose.dir == movement.DIRECTION.SOUTH then
+            pose.z = pose.z - 1
+        elseif pose.dir == movement.DIRECTION.WEST then
+            pose.x = pose.x + 1
         end
         return success
     else -- movement failed
@@ -122,7 +128,7 @@ end
 function movement.up()
     local success, msg = robot_api.up()
     if success then -- movement succeeded
-        movement.pose.y = movement.pose.y + 1
+        pose.y = pose.y + 1
         return success
     else -- movement failed
         return success, msg
@@ -132,7 +138,7 @@ end
 function movement.down()
     local success, msg = robot_api.down()
     if success then -- movement succeeded
-        movement.pose.y = movement.pose.y - 1
+        pose.y = pose.y - 1
         return success
     else -- movement failed
         return success, msg
@@ -153,14 +159,14 @@ end
 
 -- higher order movement function to move directly to target coordinates
 function movement.moveTo(target_x, target_y, target_z)
-    while (movement.pose.x ~= target_x) or (movement.pose.y ~= target_y) or (movement.pose.z ~= target_z) do
+    while (pose.x ~= target_x) or (pose.y ~= target_y) or (pose.z ~= target_z) do
         -- only one move per iteration; check if move necessary, then do it; skip all other elseif, because one was true
-        if     movement.pose.y < target_y and movement.move(movement.DIRECTION.UP)    then -- try moving up first (if necessary)
-        elseif movement.pose.x < target_x and movement.move(movement.DIRECTION.EAST)  then -- then try moving horizontally (if necessary)
-        elseif movement.pose.x > target_x and movement.move(movement.DIRECTION.WEST)  then -- ...
-        elseif movement.pose.z < target_z and movement.move(movement.DIRECTION.SOUTH) then -- ...
-        elseif movement.pose.z > target_z and movement.move(movement.DIRECTION.NORTH) then -- ...
-        elseif movement.pose.y > target_y and movement.move(movement.DIRECTION.DOWN)  then -- then try moving down (if necessary)
+        if     pose.y < target_y and movement.move(movement.DIRECTION.UP)    then -- try moving up first (if necessary)
+        elseif pose.x < target_x and movement.move(movement.DIRECTION.EAST)  then -- then try moving horizontally (if necessary)
+        elseif pose.x > target_x and movement.move(movement.DIRECTION.WEST)  then -- ...
+        elseif pose.z < target_z and movement.move(movement.DIRECTION.SOUTH) then -- ...
+        elseif pose.z > target_z and movement.move(movement.DIRECTION.NORTH) then -- ...
+        elseif pose.y > target_y and movement.move(movement.DIRECTION.DOWN)  then -- then try moving down (if necessary)
         else return false -- target not reached but all possible moves blocked
         end
     end
