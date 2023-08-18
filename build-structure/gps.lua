0c78834569c79a4d8ca05a762d073c0517c1a2f1
Maxi <42847769+M4rethyu@users.noreply.github.com>
2023-08-18 19:30:00 +0200

    test

--- build-structure/gps.lua
+++ build-structure/gps.lua
@@ -13,14 +13,7 @@ local gps = {}
 
 function gps.position()
     local wps = navigation.findWaypoints(1000) -- arg = range to search in. 1000 should cover loaded chunks
-    print(wps)
-    --[[
     for _, wp in ipairs(wps) do
-        local waypoint_x, waypoint_y, waypoint_z = string.match(wp.label, "^(%d+) (%d+) (%d+)$")
-        print(waypoint_x)
-        print(waypoint_y)
-        print(waypoint_z)--
-
         local waypoint_x, waypoint_y, waypoint_z = string.match(wp.label, "^(%d+) (%d+) (%d+)$")
         if waypoint_x and waypoint_y and waypoint_z then -- make sure waypoint label matches gps pattern
             -- waypoint.position (relative) is from view of the robot, ie. robot + wp.position = waypoint
@@ -46,8 +39,7 @@ function gps.position()
             return pose
         end
     end
-    return nil -- no matching waypoint found, so position returned
-    ]]
+    return nil -- no matching waypoint found, so no position returned
 end
 
 -- west = 4
