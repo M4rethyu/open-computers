83caad831f3cc5d173c4c0d16ec289daab4e2b5e
Maxi <42847769+M4rethyu@users.noreply.github.com>
2023-08-20 20:45:22 +0200

    faster placing order

--- build-structure/builder.lua
+++ build-structure/builder.lua
@@ -124,14 +124,21 @@ function builder.build(x_anchor, y_anchor, z_anchor, y_offset) -- skip all layer
     movement.setPose(pose.x, pose.y, pose.z, pose.dir)
 
     while y_structure < meta_data.span_y do
+        print("=== placing layer "..tostring(y_structure).." ===")
+        local layer_done = false
+
         -- read layer from bytes in file
         local layer = {} -- x-z-layer
         local line = {}  -- x-line
         local bytes = file:read(num_bytes)
         local i = 0 -- count bytes, make new x-line after span_x bytes
-        local first_non_zero -- remember first_non_zero element as starting point for placing blocks <-TODO
+        local first_non_zero -- remember first_non_zero element as starting point for placing blocks
         for b in string.gmatch(bytes, ".") do
-            table.insert(line, string.byte(b))
+            local n = string.byte(b)
+            table.insert(line, n)
+            if not first_non_zero and n ~= 0 then
+                first_non_zero = {x = i, z = #layer}
+            end
             i = i + 1
             if i == meta_data.span_x then
                 table.insert(layer, line)
@@ -140,27 +147,78 @@ function builder.build(x_anchor, y_anchor, z_anchor, y_offset) -- skip all layer
             end
         end
 
-        for _, line in ipairs(layer) do
-            for _, n in ipairs(line) do
-                io.write(tostring(n).." ")
+        x_structure = first_non_zero.x
+        z_structure = first_non_zero.z
+
+        while not layer_done do
+            local index = layer[x_structure+1][z_structure+1] -- 0-indexed blocks vs 1-indexed arrays
+            local block = block_data[index]
+
+
+            --                                               +1 because robot places downwards
+            movement.moveTo(x_anchor + x_structure, y_anchor + y_structure + 1, z_anchor + z_structure)
+            if block == "minecraft:stone" then -- todo: select correct block type in inventory
+                robot_api.placeDown()
+                print(string.format("placing block '%s' at (%d, %d)", block, x_structure, z_structure))
+                layer[x_structure+1][z_structure+1] = 0 -- block has been placed so remove it from layer data
+            else
+                --print(string.format("doing nothing with block '%s' at (%d, %d)", tostring(block), x_structure, z_structure))
             end
-            io.write("\n")
+
+
+            -- find closest non-0 block to placed block
+            local r = 0 -- radius from last placed block
+            local max_r = math.max(x_structure, meta_data.span_x - x_structure - 1, z_structure, meta_data.span_z - z_structure - 1) -- all blocks outside this radius are outside the structure
+
+            local new_point_found = false
+            while not new_point_found do -- loop until non-0 block found (place that block)
+                r = r + 1
+                if r > max_r then -- or until all blocks checked (no more blocks need to be placed)
+                    layer_done = true
+                    --print(string.format("done checking blocks around (%d, %d) because radius exceeded %d", x_structure, z_structure, max_r))
+                    break
+                end
+                --print(string.format("checking blocks around (%d, %d) at radius %d", x_structure, z_structure, r))
+
+                -- make table of all points on a square of distance r around the point
+                local points = {}
+                for i = x_structure - r, x_structure + r do
+                    table.insert(points, {x = i, z = z_structure + r})
+                    table.insert(points, {x = i, z = z_structure - r})
+                end
+                for i = z_structure - r + 1, z_structure + r - 1 do
+                    table.insert(points, {x = x_structure + r, z = i})
+                    table.insert(points, {x = x_structure - r, z = i})
+                end
+
+                for _, point in ipairs(points) do
+                    if (0 <= point.x and point.x <= meta_data.span_x - 1 and 0 <= point.z and point.z <= meta_data.span_z - 1) then -- check if point is within structure
+                        if layer[point.x+1][point.z+1] ~= 0 then -- check if placeable block at point
+                            new_point_found = true -- choose first point with placeable block as next block to be placed
+                            x_structure = point.x
+                            z_structure = point.z
+                            break
+                        end
+                    end
+                end
+            end
         end
 
+        --[=[
         local block
-        for z_structure, line in ipairs(layer) do
-            z_structure = z_structure - 1
-            for x_structure, n in ipairs(line) do
-                x_structure = x_structure - 1
+        for z_structure_, line in ipairs(layer) do
+            z_structure_ = z_structure_ - 1
+            for x_structure_, n in ipairs(line) do
+                x_structure_ = x_structure_ - 1
                 block = block_data[n]
-                movement.moveTo(x_anchor + x_structure, y_anchor + y_structure, z_anchor + z_structure)
+                movement.moveTo(x_anchor + x_structure_, y_anchor + y_structure + 1 --[[+1 because robot places downwards]], z_anchor + z_structure_)
                 if block == "minecraft:stone" then
                     robot_api.placeDown()
                 end
-                print(string.format("placing block '%s' at (%d, %d, %d)", block, x_structure, y_structure, z_structure))
+                print(string.format("placing block '%s' at (%d, %d, %d)", block, x_structure_, y_structure, z_structure_))
             end
         end
-
+        ]=]
         y_structure = y_structure + 1
     end
 
@@ -170,7 +228,7 @@ end
 
 
 builder.setInputFile("build-structure/input_.txt")
---builder.build(0, 0, 0, 0)
+builder.build(0, 0, 0, 0)
 
 
 
